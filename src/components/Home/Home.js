75d13f71905379bff967f5503d30db67d46a1cab
M-Zayn-Abbas <156469257+M-Zayn-Abbas@users.noreply.github.com>
2024-03-13 05:21:49 +0500

    built the home page , working on logo animation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,50 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png'
 import './Home.scss'
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
+import Logo from './LogoAnimation/LogoAnimation';
+
 const Home = ()=> {
+
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['l','o','b','o','d','a','n'];
+    const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.'];
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+    
+        // Cleanup function to clear the timeout when the component unmounts
+        return () => clearTimeout(timeoutId);
+    }, []);
+    
     return(
         <div className='container home-page'>
             <div className='text-zone'>
-                <h1>Hi , <br/> I,m
+                <h1>
+                  <span className={letterClass}>H</span>
+                  <span className={`${letterClass} _12`}>i,</span>
+                <br/>
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
+
                 <img src={LogoTitle} alt='develepor'/>
-                lobodan
+                <AnimatedLetters letterClass={letterClass} 
+                strArray={nameArray}
+                idx={15}/>
+                <br/>
+
+                <AnimatedLetters letterClass={letterClass} 
+                strArray={jobArray}
+                idx={22}/>
+
                 </h1>
                 <h2>Frotend develepor/ Javascript Expert</h2>
                 <Link to="contact" className="flat-button">Contact Me</Link>
             </div>
+            <Logo/>
         </div>);
 }
 
