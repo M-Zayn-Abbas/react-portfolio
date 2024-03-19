c6b18d0aec1b735af80c0855c7e1408c7944e8c5
M-Zayn-Abbas <156469257+M-Zayn-Abbas@users.noreply.github.com>
2024-03-19 22:12:15 +0500

    removed gsap

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,7 @@
 import { Link } from 'react-router-dom';
-import LogoTitle from '../../assets/images/logo-s.png'
 import './Home.scss'
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
-import Logo from './LogoAnimation/LogoAnimation';
 import Loader from 'react-loaders';
 import Zlogo from '../../assets/images/ZLogo.png'
 import Project from '../Projects/project';
@@ -49,7 +47,6 @@ const Home = ()=> {
                 <Link to="contact" className="flat-button">Contact Me</Link>
             </div>
             <Project/>
-            <Logo/>
         </div>
         <Loader type="pacman"/>
         </>);
